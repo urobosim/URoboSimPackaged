9012a154c350962b5ae62d24bb19ddf6f3e775d7
Michael Neumann <neumann.michael@uni-bremen.de>
2021-11-08 10:08:44 +0100

    remove old files

--- URoboSimPacked.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/URoboSimPackaged/Binaries/Linux/URoboSimPacked-Linux-Shipping"
-"$UE4_PROJECT_ROOT/URoboSimPackaged/Binaries/Linux/URoboSimPacked-Linux-Shipping" URoboSimPackaged $@ 
