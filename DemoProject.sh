1b34cef87c7ce68cda71bb1561e7a734b257f926
Michael Neumann <neumann.michael@uni-bremen.de>
2021-11-08 13:36:02 +0100

    update

--- DemoProject.sh
+++ DemoProject.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/DemoProject/Binaries/Linux/DemoProject-Linux-Test"
-"$UE4_PROJECT_ROOT/DemoProject/Binaries/Linux/DemoProject-Linux-Test" DemoProject "$@" 
+chmod +x "$UE4_PROJECT_ROOT/DemoProject/Binaries/Linux/DemoProject"
+"$UE4_PROJECT_ROOT/DemoProject/Binaries/Linux/DemoProject" DemoProject "$@" 
